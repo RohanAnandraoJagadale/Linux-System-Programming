1ebd6c47338115701461c89e4a8c72ff428f341d
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-07-17 20:51:28 +0530

    Add files via upload

---
+++ Assignment5/Assignment5_3.c
@@ -0,0 +1,2 @@
+// Write a program which accept directory name from user and write information of all regular file in and then read the contents from that file.
+
---
+++ Assignment5/Assignment5_5.c
@@ -0,0 +1,3 @@
+// Write a program which accept file name which contains data of all file. We have to create all file again.
+
+
