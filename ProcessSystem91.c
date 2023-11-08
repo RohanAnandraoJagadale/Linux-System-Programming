91f8b59697b0e4bc8920d8eb2e8443a738b2a89f
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-08 10:19:49 +0530

    Add files via upload

---
+++ ProcessSystem91.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+    char Filename1[30], Filename2[30];
+    int Ret = 0;
+
+    printf("Enter the existing file name : \n");
+    scanf("%s",Filename1);
+
+    printf("Enter the name of hardlink : \n");
+    scanf("%s",Filename2);
+
+    Ret = link(Filename1)
+}
