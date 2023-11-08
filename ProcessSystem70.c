51e9658f1c422e7508275ac800d7a40849956126
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-08 10:16:53 +0530

    Add files via upload

---
+++ ProcessSystem70.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<pthread.h>
+
+
+// void *______(void * ______)  prototype
+// {}
+
+void * ThreadProc(void *ptr)    // call back function
+{
+    int i = 0;
+
+    for(i = 1; i < 1000; i++)
+    {
+        printf("Thread with counter : %d\n",i);
+    }    
+}
+
+int main()
+{
+    pthread_t TID;
+    int ret = 0;
+
+    ret = pthread_create(&TID,    //Address of pthread_attr_t structure object // id return value hetnar
+                         NULL,    // Thread attributes                         //
+                         ThreadProc,  // Address of callback function          // thread create jhalyav treadproc la call karnar
+                         NULL);    // Parametes to callback function           //
+
+    if(ret != 0)
+    {
+        printf("Unable to create thread\n");
+        return -1;
+    }
+
+    printf("Thread is created with ID : %d\n",TID);
+
+    pthread_join(TID,NULL);
+
+    printf("End of main thread\n");
+
+    return 0;
+}
