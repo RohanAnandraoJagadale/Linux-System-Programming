df0021031752fd823ffe35a581f9072f58ace5c8
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-08 10:09:40 +0530

    Add files via upload

---
+++ ProcessSystem43.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+    int PID = 0;
+    int PPID = 0;
+
+    PID = getpid();
+    printf("PID of running process is : %d\n",PID);
+
+    PPID = getppid();
+    printf("PPID of parent process is : %d\n",PPID);  
+    // terminal cha PPID
+
+
+    return 0;
+}
