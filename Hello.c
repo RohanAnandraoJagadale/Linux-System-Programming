f5872caebaceeab8bbee1c9377ee4d30d1486952
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-08 10:08:19 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int Arr[5000] = {10,20,30,40,50,60,70,80};
+
+int main()
+{
+
+    int Brr[5000];
+
+
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+    printf("Hello world..\n");
+
+    char *p = (char *)malloc(10000);
+
+    return 0;
+}
