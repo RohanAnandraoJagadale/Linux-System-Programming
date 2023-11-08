5627af4d3ff227cef93d425e5650196255a8aa0a
RohanAnandraoJagadale <114255962+RohanAnandraoJagadale@users.noreply.github.com>
2023-11-08 15:54:50 +0530

    Add files via upload

---
+++ FileSystem/fileno.c
@@ -0,0 +1,33 @@
+/*
+	This program demonsate how to retrive file descrptor from th FILE pointer
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	FILE *fp;
+	int fd;
+
+	// Open file
+	fp = fopen("file.txt","r");
+	if(fp == NULL)
+	{
+		printf("Unable to open the file\n");
+		return -1;
+	}
+
+	// This function returns file descripter which is allocated for the file.
+	
+	fd = fileno(fp);  // fd anun deto, ekmev function system caal aani function karTo
+	// First parameter :	File pointer
+	if(fd == -1)
+	{
+		printf("Unable to get file descriptor\n");
+		return -1;
+	}
+
+	printf("File descriptor is %d\n",fd);
+
+	return 0;
+}
